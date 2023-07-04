3bd4835e2e8d2f8c300d6997d2587fb1261ce282
bernard0ts <bernardodasilva05@gmail.com>
2023-07-04 18:04:38 -0300

    {WIP} devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  get 'pages/home'
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    registrations: 'admin/registrations'
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
+  # root 'pages#home'
   get 'pages/users'
   get 'pages/admin'
   resources :apartments
