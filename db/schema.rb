f1d5e90d9a6e232f175b65809aa8b5d448bc32be
bernard0ts <bernardodasilva05@gmail.com>
2023-07-19 18:04:28 -0300

    {WIP in show admin name when clicking apartment.} Changes when clicking show and indented code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_205906) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_180937) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_205906) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
